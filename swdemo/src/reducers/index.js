22d2cbe7933fa8ca34bc5825ef24293fa57e34cd
Pablo Munoz <pmunoz@skycatch.com>
2019-04-05 09:17:18 -0600

    Remove other demo from code

--- swdemo/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import peopleReducer from './peopleReducer';
-import planetsReducer from './planetsReducer';
-
-export default combineReducers({
-  peopleState: peopleReducer,
-  planetsState: planetsReducer
-});
